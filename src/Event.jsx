0812a2f126df02eda0f7f609041fafe728e2e00c
tarikbayer <tarikfiratbayer@gmail.com>
2025-03-16 14:05:40 +0300

    Initialized Timeline and Event style sheets

--- src/Event.jsx
+++ src/Event.jsx
@@ -1,9 +1,11 @@
-function Event(props) {
+import "./styles/Event.css";
+
+function Event({ title, year, description }) {
   return (
     <div>
-      <h3>{props.name}</h3>
-      <h4>{props.year}</h4>
-      <p>{props.description}</p>
+      <h3>{title}</h3>
+      <h4>{year}</h4>
+      <p>{description}</p>
     </div>
   );
 }
--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -1,18 +1,39 @@
 import Event from "./Event";
+import "./styles/Timeline.css";
 
 function Timeline() {
   return (
-    <div>
-      <Event
-        name={"VAR"}
-        year={"2020"}
-        description={"Video Assistant Referee was introduced during EURO 2021"}
-      />
-      <Event
-        name={"Semi Automatic Offside Tech"}
-        year={"2018"}
-        description={"It came during UCL Final 2020"}
-      />
+    <div className="timeline">
+      <div className="yearContainer">
+        <h2 className="yearHeader">2022</h2>
+        <Event
+          title={"Semi Automatic Offside Tech"}
+          year={"2022"}
+          description={"It came during 2022 World  Cup in Qatar"}
+        />
+        <Event
+          title="First Event"
+          year="2022"
+          description="Something happened"
+        />
+        <Event
+          title="Second Event"
+          year="2022"
+          description="Another event in 2022"
+        />
+      </div>
+      <div className="yearContainer">
+        <div className="yearHeader">
+          <h2>2018</h2>
+          <Event
+            title={"VAR"}
+            year={"2018/19"}
+            description={
+              "The use of video assistant referees (VARs) in football was first included in the Laws of the Game in 2018/19. After the 2016 introduction in CUP football in Europe, the VAR system was introduced in top-flight European football league competitions by Bundesliga and the Serie A at the beginning of the 2017–18 season and by La Liga at the beginning of the 2018–19 season."
+            }
+          />
+        </div>
+      </div>
     </div>
   );
 }
